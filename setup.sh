8a9735c62adcdf9af29a0f74a593e1bcf91fec89
Kensuke Saito <ken.saito.0813@gmail.com>
2022-09-21 00:14:24 +0900

    Fixed setup.sh to remove existing docker-rust-builder in /tmp

--- setup.sh
+++ setup.sh
@@ -4,7 +4,8 @@ rust-builder() {
         && rm -rf /tmp/docker-rust-builder/.git \
             /tmp/docker-rust-builder/LICENSE \
             /tmp/docker-rust-builder/setup.sh \
-        && cp -r /tmp/docker-rust-builder/* ./
+        && cp -r /tmp/docker-rust-builder/* ./ \
+        && rm -rf /tmp/docker-rust-builder
 
     return $?
 }
