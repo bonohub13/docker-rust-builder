a97337292c7aaea9015be984506590dd282bc606
Kensuke Saito <ken.saito.0813@gmail.com>
2022-09-21 23:57:57 +0900

    modified setup.sh to exclude README.md from project

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ rust-builder() {
     git clone https://github.com/bonohub13/docker-rust-builder /tmp/docker-rust-builder \
         && rm -rf /tmp/docker-rust-builder/.git \
             /tmp/docker-rust-builder/LICENSE \
+            /tmp/docker-rust-builder/README.md \
             /tmp/docker-rust-builder/setup.sh \
         && cp -r /tmp/docker-rust-builder/* ./ \
         && rm -rf /tmp/docker-rust-builder
